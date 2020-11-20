aaecfdcc05b2cbb68f1a77c3589acf6b38bf39f6
Gisellemfc <Gisellemfc@users.noreply.github.com>
2020-11-19 20:52:22 -0400

    Metodo suma y cambio de nombre doc

---
+++ Calculadora-Brito-Ferreira.asm
@@ -0,0 +1,225 @@
+#INCLUIR EL ARCHIVO DE LOS MACROS EN EL PROYECTO
+.include "macros.asm"
+
+
+#SECCIÓN DE DATOS EN MEMORIA
+.data
+
+#MENSAJES DE INICIO DEL PROGRAMA
+titulo:		.asciiz "------------------------------------------------|    CALCULADORA BASICA    |------------------------------------------------\n\n"
+encabezado:	.asciiz "      Este proyecto fue elaborado por Nicole Brito y Giselle Ferreira, estudiantes de la Universidad Metropolitana de\n      Caracas - Venezuela, en la asignatura de Organización del Computador, dictada por el profesor Rafael Matienzo.\n"	
+descripcion:	.asciiz "\nDescripción: Este es un programa que realiza la suma, la resta y la multiplicación de números enteros largos. Dichos \nenteros no caben en registros, sino que se representan como cadenas de caracteres de hasta 50 caracteres, todos los \ncuales son caracteres ASCII numéricos, excepto el primero que es un signo (+ o -).\n\n"
+instrucciones:	.asciiz "Instrucciones:\n1) Las operaciones se realizan con números de hasta 50 dígitos, incluyendo el signo. \n2) Los números que ingrese deben incluir obligatoriamente como primer dígito un signo (+) si el número es positivo o un \nsigno (-) si el número es negativo.\n3)Al introducir un dato erróneo, recibirá un mensaje de error y se le solicitarán los datos nuevamente.\n"
+separador:	.asciiz "\n----------------------------------------------------------------------------------------------------------------------------\n"	
+
+#MENSAJES DE ERROR
+opcion_invalida:	.asciiz "\nOpción inválida... Inténtalo nuevamente"
+numero_invalido:	.asciiz "\nNúmero inválido... Inténtalo nuevamente"
+
+#MENÚ DE OPERACIONES
+menu:		.asciiz "\nMenú: \n\n(1) Sumar \n(2) Restar \n(3) Multiplicar \n\nIngrese el número de la operación que desea realizar: "
+
+#MENSAJES PARA PEDIR NUMEROS
+solicitud1:	.asciiz "\nIngrese el primer número: "
+solicitud2:	.asciiz "\nIngrese el segundo número: "
+
+#MENSAJES DEL RESULTADO DE LAS OPERACIONES
+res:		.asciiz "\nResultado: "
+num1:		.asciiz "\nNúmero 1: "
+num2:		.asciiz "\nNúmero 2: "
+
+#SIGNOS
+mas:		.asciiz " + "
+menos:		.asciiz " - "
+por:		.asciiz " x "
+igual:		.asciiz " = "
+
+#ESPACIOS DE MEMORIA RESERVADOS PARA LOS STRINGS A UTILIZAR
+numero1:		.space 51
+numero2:		.space 51
+resultado:	.space 55
+resultado_mult:	.space 105
+
+#SECCIÓN DE CÓDIGO MAIN
+.text
+
+
+#INICIALIZAR REGISTROS A USAR
+li $t0, 0 	#OPCIÓN SELECCIONADA DEL MENÚ DE OPERACIONES
+li $t1, 0		#PRIMER NÚMERO
+li $t2, 0		#SEGUNDO NÚMERO
+li $t3, 0		#RESULTADO
+li $t4, 1		#SIGNO DEL PRIMER NUMERO	
+li $t5, 1		#SIGNO DEL SEGUNDO NUMERO
+li $t6, 0		#TAMAÑO DEL PRIMER NUMERO 
+li $t7, 0		#TAMAÑO DEL SEGUNDO NUMERO 
+li $t8, 0		#GUARDA CUAL ES EL NÚMERO MAYOR
+
+		
+#MENSAJES DE INICIO DEL PROGRAMA
+mensaje_string(titulo)		#TITULO DE LA CALCULADORA
+mensaje_string(encabezado)		#ENCABEZADO DEL PROGRAMA
+mensaje_string(separador)		#LÍNEA SEPARADORA	
+mensaje_string(descripcion)		#DESCRIPCIÓN DEL PROGRAMA
+mensaje_string(instrucciones)		#INSTRUCCIONES DEL PROGRAMA
+mensaje_string(separador)		#LÍNEA SEPARADORA
+	
+	
+#LOOP DE INSERTAR OPCIONES DEL MENÚ CON VALIDACIÓN	
+Insertar_Opciones:	
+		mensaje_string(menu)		#MENÚ DE OPCIONES
+		pedir_entero($t0)			#PEDIR OPCIÓN DEL MENÚ
+		mensaje_string(separador)		#LÍNEA SEPARADORA
+		
+		ble $t0, 0, error			#SI EL NUMERO ES MENOR QUE 1 MENSAJE DE ERROR
+		bgt $t0, 3, error			#SI EL NUMERO ES MAYOR A 3 MENSAJE DE ERROR
+		b Insertar_Datos			#SI EL NUMERO ESTÁ BIEN, PEDIR LOS DATOS
+		
+		
+# SECCIÓN DE CÓDIGO DE MENSAJE DE ERROR			
+error:			
+		mensaje_string(opcion_invalida)	#MENSAJE DE ERROR
+		mensaje_string(separador)		#LÍNEA SEPARADORA
+		b Insertar_Opciones			#VOLVER A PEDIR LA OPCIÓN DEL MENÚ
+
+
+#SECCIÓN DE CODIGO PARA INSERTAR LOS NUMEROS A OPERAR
+Insertar_Datos:
+	#LOOP DE INSERTAR EL PRIMER NUMERO CON VALIDACIÓN
+	Insertar_Num1:		
+		mensaje_string(solicitud1)						#MENSAJE PEDIR PRIMER NÚMERO
+		pedir_string(numero1)						#GUARDAR NUMERO 1 EN MEMORIA
+		validar_numero(numero1, $t4, Insertar_Num1, numero_invalido, $t1, $t6)	#VALIDAR EL NUMERO 1
+
+	#LOOP DE INSERTAR EL SEGUNDO NUMERO CON VALIDACIÓN
+	Insertar_Num2:	
+		mensaje_string(solicitud2)						#MENSAJE PEDIR EL SEGUNDO NUMERO
+		pedir_string(numero2)						#GUARDAR EL NUMERO 2 EN MEMORIA
+		validar_numero(numero2, $t5, Insertar_Num2, numero_invalido, $t2, $t7)	#VALIDAR EL NUMERO 2
+	
+							
+#CONDICIONAL DE LOS CASOS	
+beq $t0, 1, suma			#IR AL CASO DE LA OPERACIÓN SUMA
+beq $t0, 2, resta			#IR AL CASO DE LA OPERACION RESTA
+beq $t0, 3, multiplicacion		#IR AL CASO DE LA OPERACIÓN MULTIPLICACIÓN
+
+
+#CASOS DE LA SUMA
+suma:
+	
+		# SI ESTAMOS EN LA SUMA:
+		#
+		# 1) CALCULAR EL TAMAÑO DE AMBOS NUMEROS
+		# 2) IGUALAR EL TAMAÑO DE AMBOS NUMEROS
+		# 3) CALCULAR CUAL DE LOS DOS NUMEROS ES EL MAYOR
+		# 4) CASOS:
+		#
+		#    - SI LOS DOS SON DEL MISMO SIGNO, ENTONCES ACTUA COMO SUMA:
+		#	* SE HACE LA SUMA CON EL MAYOR ARRIBA
+		#	* TOMA EL SIGNO DEL NUMERO MAYOR
+		#
+		#    - SI LOS DOS SON DE DISTINTO SIGNO, ENTONCES ACTUA COMO RESTA:
+		#	* SE HACE LA RESTA CON EL MAYOR ARRIBA
+		#	* TOMA EL SIGNO DEL NUMERO MAYOR
+	
+	calcular_mayor				#CALCULAMOS CÚAL NUMERO ES MAYOR
+	igualar_tamano(numero1, numero2, $t6, $t7)	#IGUALAR LOS TAMAÑOS DE LOS NÚMEROS	
+
+	
+	#SI LOS DOS NUMEROS SON DEL MISMO SIGNO
+	beq $t4, $t5, hacer_suma
+	#SI LOS DOS NUMEROS SON DE DISTINTO SIGNO
+	bne $t4, $t5, hacer_suma_restada
+	
+	hacer_suma:
+		sumar(numero1, numero2, resultado, $t6)
+		beqz $t4, imprimir_menos_suma
+		
+				mensaje_string(mas)
+				mensaje_string(resultado)
+				b final
+		
+		imprimir_menos_suma:
+				mensaje_string(menos)
+				mensaje_string(resultado)
+				b final
+	
+	
+	
+	
+	
+	hacer_suma_restada:
+	
+	b final
+
+
+#CASOS DE LA RESTA
+resta:
+
+	# SI ESTAMOS EN LA RESTA:
+	#
+	# 1) CALCULAR EL TAMAÑO DE AMBOS NUMEROS
+	# 2) IGUALAR EL TAMAÑO DE AMBOS NUMEROS
+	# 3) CALCULAR CUAL DE LOS DOS NUMEROS ES EL MAYOR
+	# 4) CASOS:
+	#
+	#    - SI LOS DOS SON DEL MISMO SIGNO, ENTONCES ACTUA COMO RESTA:
+	#	* SE HACE LA RESTA CON EL MAYOR ARRIBA
+	#	* TOMA EL SIGNO DEL NUMERO MAYOR
+	#
+	#    - SI LOS DOS SON DE DISTINTO SIGNO, ENTONCES ACTUA COMO SUMA:
+	#	* SE HACE LA SUMA CON EL MAYOR ARRIBA
+	#	* TOMA EL SIGNO DEL NUMERO MAYOR
+
+	calcular_mayor
+	igualar_tamano(numero1, numero2, $t6, $t7)
+	
+	
+	#SI LOS DOS NUMEROS SON DEL MISMO SIGNO
+	beq $t4, $t5, hacer_resta
+	#SI LOS DOS NUMEROS SON DE DISTINTO SIGNO
+	bne $t4, $t5, hacer_resta_sumada
+	
+	hacer_resta:
+	
+	b final
+	
+
+	
+	hacer_resta_sumada:
+		sumar(numero1, numero2, resultado, $t6)
+		
+		beqz $t8, num1_mayor
+		
+		beqz $t5, imprimir_menos_resta_sumada_num2
+		
+				mensaje_string(mas)
+				mensaje_string(resultado)
+				b final
+		
+		imprimir_menos_resta_sumada_num2:
+				mensaje_string(menos)
+				mensaje_string(resultado)
+				b final
+		
+		num1_mayor:
+		beqz $t4, imprimir_menos_resta_sumada_num1
+		
+				mensaje_string(mas)
+				mensaje_string(resultado)
+				b final
+		
+		imprimir_menos_resta_sumada_num1:
+				mensaje_string(menos)
+				mensaje_string(resultado)
+				b final
+
+
+#CASOS DE LA MULTIPLICACION
+multiplicacion:
+
+
+#SALTO AL FINALIZAR LA OPERACIÓN
+final:
+		
+	fin 	#MACRO QUE FINALIZA EL PROGRAMA
--- Macros.asm
+++ Macros.asm
@@ -2,86 +2,75 @@
 
 .text
 
+
+
 #MACRO QUE FINALIZA EL PROGRAMA
 .macro fin
 	li $v0, 10
 	syscall
-	.end_macro 
-	
+.end_macro 
+
+		
+
 #MACRO QUE IMPRIME MENSAJE
 .macro mensaje_string (%text)
 	li   $v0,4
 	la   $a0, %text
 	syscall
-	.end_macro 
+.end_macro 
+
+		
 	
 #MACRO QUE PIDE UN ENTERO
 .macro pedir_entero (%registro)
 	li  $v0, 5
 	syscall
 	move %registro, $v0
-	.end_macro 
+.end_macro 
+	
+	
 	
 #MACRO QUE PIDE UN STRING
 .macro pedir_string (%direccion)
 	li $v0, 8
 	la $a0, %direccion
-	la $a1, 51
+	la $a1, 50
 	syscall
-	.end_macro 
+.end_macro 
 	
-#MACRO PARA LIMPIAR NUMERO EN MEMORIA INCORRECTO
+	
+	
+#MACRO PARA LIMPIAR NUMERO EN MEMORIA SI ES INCORRECTO
 .macro clean_numero(%direccion, %tamano)
 
-li $s1, 0x00	#ASIGNA NULL A $S1
+li $s1, 0x00		#ASIGNA NULL A $S1
 
 #LOOP PARA VACIAR EL ESPACIO DE MEMORIA
 clean_numero: 	 			
-		sb   $s1, %direccion($s0)	#METE NULL EN LA POSICION DEL DIGITO
-		subi $s0, $s0, 1		#AUMENTAR UNO AL APUNTADOR
-		bgez $s0, clean_numero	#RECORRER COMPLETO EL ESPACIO DE MEMORIA
-	.end_macro 
+		sb $s1, %direccion(%tamano)		#METE NULL EN LA POSICION DEL DIGITO
+		subi %tamano, %tamano, 1		#RETROCEDER UNA POSICION AL APUNTADOR
+		bgez %tamano, clean_numero		#RECORRER EL ESPACIO DE MEMORIA HASTA EL INICIO
+.end_macro 
 	
-#MACRO PARA QUITAR EL SIGNO DEL NUMERO EN MEMORIA (SHIFT A LA IZQUIERDA DEL NUMERO)
-.macro quitar_signo (%direccion, %tamaño)
-li $s1, 1 	#APUNTADOR POSICION SIGUIENTE
-li $s2, 0 	#APUNTADOR POSICION ACTUAL
-li $t9, 0x00	#ASIGNADOR DE NULL A ESPACIOS LIBRES
-
-loop_shift:	
-		lb   $t8, %direccion($s1)		#AGARRO EL NUMERO DE LA POSICION SIGUIENTE
-		sb   $t9, %direccion($s1)		#PONGO NULL A ESA POSICION
-		sb   $t8, %direccion($s2)		#PONGO EL NUMERO DE LA POSICION SIGUIENTE EN LA POSICION ACTUAL
-		addi $s1, $s1, 1			#AUMENTO APUNTADOR SIGUIENTE
-		addi $s2, $s2, 1			#AUMENTO APUNTADOR ACTUAL
-		blt  $s2, %tamaño, loop_shift		#SE REPITE HASTA QUE RECORRE TODO EL NUMERO
-	.end_macro 
 	
-#MACRO PARA VALIDAR QUE EL NUMERO INGRESADO COMO STRING SEA UN NUMERO POSITIVO O 
-#NEGATIVO DE HASTA 50 DIGITOS SI ES SUMA O RESTA Y DE HASTA 25 DIGITOS SI ES MULTIPLICACIÓN
-.macro validar_numero (%direccion_numero, %registro_signo, %salto_error, %mensaje,%registro_numero)
+	
+#MACRO PARA VALIDAR QUE EL NUMERO INGRESADO COMO STRING SEA UN NUMERO POSITIVO O NEGATIVO DE HASTA 50 DIGITOS
+.macro validar_numero (%direccion_numero, %registro_signo, %salto_error, %mensaje,%registro_numero, %registro_tamano)
 
 li $s0, 0			#APUNTADOR PARA RECORRER LA VALIDACIÓN DEL NUMERO
 
 #LOOP QUE RECORRE EL STRING DEL NUMERO
 loop_numero:		
 		lb   %registro_numero, %direccion_numero($s0) 	#TOMAMOS UN DIGITO DEL STRING
-		beqz $s0, validacion_digito
+		beqz $s0, validacion_digito			#SI ES EL PRIMER DÍGITO ENTONCES VALIDAMOS SI ES EL SIGNO
 		blt  %registro_numero, 0x30, validacion_digito	#SI EL DIGITO ES MENOR QUE 0X30 (0) ENTONCES VALIDARLO
 		bgt  %registro_numero, 0x39, validacion_digito	#SI EL DIGITO ES MAYOR QUE 0X39 (9) ENTONCES VALIDARLO
 
-validacion_apuntador:		
-		addi $s0, $s0, 1			#AUMENTAR UNO AL APUNTADOR QUE RECORRE EL NUMERO
-	  	bne  $t0, 3, validacion_no_pasar_50	#SI LA OPERACION ES SUMA O RESTA ENTONCES VALIDAR TAMAÑO DEL STRING MENOR A 50
-	  	beq  $t0, 3, validacion_no_pasar_25	#SI LA OPERACION ES MULTIPLICACIÓN ENTONCES VALIDAR TAMAÑO MENOR A 25
-
-validacion_no_pasar_50:		
-		bgt  $s0, 50, error_digito		#SI ES SUMA O RESTA Y EL APUNTADOR ES MAYOR A 50 ENTONCES ERROR
+		validacion_apuntador:		
+		addi $s0, $s0, 1			#AUMENTAR UNO AL APUNTADOR QUE RECORRE EL NUMERO		
+		bgt  $s0, 50, error_digito		#VALIDAR SI EL NUMERO ES MAYOR A 50 DIGITOS ENTONCES ERROR
 		j loop_numero			#SEGUIR VALIDADNDO EL NUMERO
 
-validacion_no_pasar_25:		
-		bgt  $s0, 25, error_digito		#SI ES MULTIPLICACIÓN Y EL APUNTADOR ES MAYOR A 25 ENTONCES ERROR
-		j loop_numero			#SEGUIR VALIDANDO EL NUMERO
 
 #PORCION DE CODIGO QUE VALIDA SI SE INGRESO UNA LETRA U OTRA COSA
 validacion_digito:
@@ -90,39 +79,114 @@ validacion_digito:
 		beq  %registro_numero, 0x0a, fin_numero		#SI SE LLEGO AL FINAL DEL STRING ENTONCES TERMINAR LA VALIDACIÓN
 		b error_digito			#SI NO CUMPLE NADA DE LO ANTERIOR ENTONCES IR A ERROR
 		
+		
 #PORCIÓN DE CÓDIGO QUE GUARDA SI EL NUMERO ES NEGATIVO
 numero_negativo:	
-		bgtz $s0, error_digito	#SI EL APUNTADOR NO ESTÁ EN LA PRIMERA POSICIÓN, ENTONCES MOSTRAR ERROR, PORQUE ES UNA CARACTER INCORRECTO EN MEDIO DEL NUMERO		
-		li %registro_signo, 0	#CAMBIAR EL REGISTRO DEL SIGNO A 0
+		bgtz $s0, error_digito	#SI EL APUNTADOR NO ESTÁ EN LA PRIMERA POSICIÓN, ENTONCES MOSTRAR ERROR, PORQUE ES UN CARACTER INCORRECTO EN MEDIO DEL NUMERO		
+		li %registro_signo, 0	#CAMBIAR EL REGISTRO DEL SIGNO A 0 (-)
 		j validacion_apuntador	#SEGUIR VALIDANDO
+		
 		
 #PORCIÓN DE CÓDIGO QUE GUARDA SI EL NUMERO ES POSITIVO
 numero_positivo:	
-		bgtz $s0, error_digito	#SI EL APUNTADOR NO ESTÁ EN LA PRIMERA POSICIÓN, ENTONCES MOSTRAR ERROR, PORQUE ES UNA CARACTER INCORRECTO EN MEDIO DEL NUMERO		
-		j validacion_apuntador	#SEGUIR VALIDANDO	
+		bgtz $s0, error_digito	#SI EL APUNTADOR NO ESTÁ EN LA PRIMERA POSICIÓN, ENTONCES MOSTRAR ERROR, PORQUE ES UN CARACTER INCORRECTO EN MEDIO DEL NUMERO		
+		j validacion_apuntador	#SEGUIR VALIDANDO, PORQUE POR DEFECTO EL REGISTRO DEL SIGNO ES (+)	
+
 
 #PORCION DE CODIGO QUE MUESTRA ERROR
 error_digito:
 		mensaje_string(%mensaje)		#MENSAJE DE ERROR
+		mensaje_string(separador)		#LÍNEA SEPARADORA
 		clean_numero(%direccion_numero, $s0)	#VACIAR EL ESPACIO DE MEMORIA DONDE ESTABA EL NUMERO
 		li $s0, 0				#REINICIAR EL APUNTADOR DEL NUMERO
 		b %salto_error			#SALTAR A DONDE VUELVE A PEDIR DATO
 
+
 #PORCION DE CODIGO QUE SE EJECUTA AL TERMINAR DE REVISAR EL NUMERO
 fin_numero:		
-		quitar_signo (%direccion_numero, $s0)		#SHIFT A LA IZQUIERDA PARA QUITAR EL SIGNO DEL STRING
-	.end_macro 
-	
-.macro sumar(%numero1, %numero2, %resultado)
+		subi $s0, $s0, 2				#SE QUITA 2 PARA DESCONTAR EL SIGNO Y QUE APUNTA A NULL	
+		move %registro_tamano, $s0			#SE GUARDA EL TAMAÑO DEL NUMERO EN UN REGISTRO
+		addi $s0, $s0, 2				#SE DEJA EL APUNTADOR DONDE ESTABA ANTES
+		shift_izquierda (%direccion_numero, $s0)		#SHIFT A LA IZQUIERDA PARA QUITAR EL SIGNO DEL STRING
+.end_macro 
 
-	li $s0, 0		#REGISTRO QUE ALMACENA EL ACARREO DE LA SUMA
-																									
-	move $s1, $a1	#TAMAÑO DEL NUMERO
+
+
+#MACRO PARA IGUALAR LOS TAMAÑOS DE LOS NUMEROS SI ES SUMA
+.macro igualar_tamano(%numero1, %numero2, %tamano1, %tamano2)
+
+beq %tamano1, %tamano2, final_relleno
+blt %tamano1, %tamano2, rellenar_numero1
+blt %tamano2, %tamano1, rellenar_numero2
+
+rellenar_numero1:
+		shift_derecha(%numero1, %tamano1)
+		addi %tamano1, %tamano1, 1
+		bne %tamano1, %tamano2, rellenar_numero1
+		b final_relleno
+
+rellenar_numero2:
+		shift_derecha(%numero2, %tamano2)
+		addi %tamano2, %tamano2, 1
+		bne %tamano1, %tamano2, rellenar_numero2
+		b final_relleno
+
+final_relleno:
+.end_macro 
+
+
+
+#MACRO PARA CALCULAR EL MAYOR SI ES RESTA
+.macro calcular_mayor()
+
+li $s0, 0				#APUNTADOR AL PRINCIPIO DEL NÚMERO
+
+beq $t6, $t7, comparar		#SI LOS NUMEROS TIENEN EL MISMO TAMAÑO ENTONCES COMPARAR PRIMER DÍGITO
+blt $t6, $t7, numero2_mayor		#SI EL NUMERO 2 ES MÁS GRANDE
+blt $t7, $t6, numero1_mayor		#SI EL NUMERO 1 ES MÁS GRANDE
+
+
+#PORCIÓN DE CÓDIGO QUE COMPARA EL PRIMER DÍGITO DE LOS NUMEROS
+comparar:
+	
+	lb  $t1, numero1($s0)	#AGARRO EL PRIMER DÍGITO DEL NÚMERO 1
+	and $t1, $t1, 0x000f	
+	lb  $t2, numero2($s0)	#AGARRO EL PRIMER DÍGITO DEL NÚMERO 2
+	and $t2, $t2, 0x000f
+							
+	blt $t1, $t2, numero2_mayor	#SI EL ULTIMO DÍGITO DEL NUMERO 2 ES MAYOR ENTONCES NUMERO 2 ES MÁS GRANDE
+	blt $t2, $t1, numero1_mayor	#SI EL ULTIMO DÍGITO DEL NUMERO 1 ES MAYOR ENTONCES NUMERO 1 ES MÁS GRANDE
+	
+	
+#PORCIÓN DE CODIGO SI EL NUMERO 1 ES MAYOR
+numero1_mayor:
+	li $t8, 0			#CAMBIAMOS EL REGISTRO A NÚMERO 1 ES EL MAYOR
+	b final_calcular_mayor	#SALTAMOS AL FINAL
+
+	
+#PORCIÓN DE CODIGO SI EL NUMERO 2 ES MAYOR	
+numero2_mayor:
+	li $t8, 1			#CAMBIAMOS EL REGISTRO A NÚMERO 1 ES EL MAYOR
+	b final_calcular_mayor	#SALTAMOS AL FINAL
+	
+final_calcular_mayor:
+.end_macro 
+
+	
+			
+#MACRO PARA HACER LA SUMA DE LOS DOS NUMERO
+.macro sumar(%numero1, %numero2, %resultado, %tamano)
+
+	li $s0, 0		#REGISTRO QUE ALMACENA EL ACARREO DE LA SUMA																							
+	move $s1, %tamano	#TAMAÑO DEL NUMERO
 	
 loopsuma:			
 	lb   $t1,%numero1($s1)	#AGARRAR EL ULTIMO DIGITO DEL NUMERO 1	
 	lb   $t2, %numero2($s1)	#AGARRAR EL ULTIMO DIGITO DEL NUMERO 2
-			
+	
+	subi $t1, $t1, 0x30
+	subi $t2, $t2, 0x30
+	
 	add  $t3,$t2,$t1		#SUMA LOS DOS NUMEROS		
 	add  $t3,$t3,$s0		#SUMA EL ACARREO
 				
@@ -137,35 +201,61 @@ loopsuma3:
 	subi $s1, $s1,1		#SE MUEVE EL APUNTADOR AL NUMERO DE LA IZQUIERDA
 			
 	bgez $s1, loopsuma		#MIENTRAS EL APUNTADOR SEA MAYOR A 0 SEGUIR SUMANDO
-	
-bnez $s0, acarreo_final		#SI QUEDA ACARREO CUANDO SE TERMINAN DE SUMAR LOS DOS NUMEROS ENTONCES HAY QUE METER EL ACARREO EN EL NUMERO
+
+		
+beqz $s0, final		#SI NO QUEDA ACARREO CUANDO SE TERMINAN DE SUMAR LOS DOS NUMEROS ENTONCES HAY QUE METER EL ACARREO EN EL NUMERO
+
+
+#PORCIÓN DE CÓDIGO QUE MANEJA EL ACARREO FINAL DEL NÚMERO
 acarreo_final:
-	meter_acarreo_final	
+	shift_derecha(%resultado, %tamano)	
+	li   $t3, 0x31		#COMO EL ACARREO NO PUEDE SER MAYOR A 1, EL ACARREO VALE 1
+	sb   $t3, resultado($s0)	#GUARDA EL ACARREO DEL PRINCIO (FINAL)
 b final
+
+
 #PARTE DE CODIGO QUE MANEJA EL ACARREO
 acarreo:			
 	subi $t3,$t3,10	#SE LE QUITA 10 A $t3 PORQUE NINGUN ACARREO LLEGA A 20
 	li $s0,1		#SE PONE EL ACARREO EN 1
 	b loopsuma2	#SE SIGUE SUMANDO
 	
+	
 final:
 .end_macro 
 	
-#MACRO PARA METER EL ULTIMO DIGITO DEL ACARREO EN EL NUMERO (SHIFT A LA DERECHA)
-.macro meter_acarreo_final()
+	
+	
+#MACRO PARA METER EL ULTIMO DIGITO DEL ACARREO EN EL NUMERO (SHIFT A LA DERECHA DEL NUMERO)
+.macro shift_derecha(%direccion, %tamano)
 
-addi $s0, $a1, 1		#APUNTADOR AL ESPACIO SIGUIENTE
-move $s1, $a1		#APUNTADOR AL ESPACIO ACTUAL
+addi $s0, %tamano, 1		#APUNTADOR AL ESPACIO SIGUIENTE
+move $s1, %tamano		#APUNTADOR AL ESPACIO ACTUAL
 li $s2, 0x30		#CERO
 			
 loop_shift:			
-	lb   $t3, resultado($s1)	#GUARDA EN $t3 EL DIGITO ACTUAL
-	sb   $s2, resultado($s1)	#CAMBIA EL DIGITO ACTUAL EN MEMORIA A 0
-	sb   $t1, resultado($s0)	#PONE EL DIGITO ACTUAL EN LA POSICION SIGUIENTE
+	lb   $s3, %direccion($s1)	#GUARDA EN $s3 EL DIGITO ACTUAL
+	sb   $s2, %direccion($s1)	#CAMBIA EL DIGITO ACTUAL EN MEMORIA A 0
+	sb   $s3, %direccion($s0)	#PONE EL DIGITO ACTUAL EN LA POSICION SIGUIENTE
 	subi $s1, $s1, 1		#LE RESTA 1 AL APUNTADOR ACTUAL
 	subi $s0, $s0, 1		#LE RESTA 1 AL APUNTADOR SIGUIENTE
 	bgez $s1, loop_shift	#REPITE EL SHIFT HASTA QUE EL APUNTADOR ACTUAL LLEGA AL PRINCIPIO DEL NUMERO
-	
-li   $t3, 0x31		#COMO EL ACARREO NO PUEDE SER MAYOR A 1, EL ACARREO VALE 1
-sb   $t3, resultado($s0)	#GUARDA EL ACARREO DEL PRINCIO (FINAL)
-	.end_macro 
+
+.end_macro 
+
+
+#MACRO PARA QUITAR EL SIGNO DEL NUMERO EN MEMORIA (SHIFT A LA IZQUIERDA DEL NUMERO)
+.macro shift_izquierda (%direccion, %tamaño)
+
+li $s1, 1 	#APUNTADOR POSICION SIGUIENTE
+li $s2, 0 	#APUNTADOR POSICION ACTUAL
+li $t9, 0x00	#ASIGNADOR DE NULL A ESPACIOS LIBRES
+
+loop_shift:	
+		lb   $t8, %direccion($s1)		#AGARRO EL NUMERO DE LA POSICION SIGUIENTE
+		sb   $t9, %direccion($s1)		#PONGO NULL A ESA POSICION
+		sb   $t8, %direccion($s2)		#PONGO EL NUMERO DE LA POSICION SIGUIENTE EN LA POSICION ACTUAL
+		addi $s1, $s1, 1			#AUMENTO APUNTADOR SIGUIENTE
+		addi $s2, $s2, 1			#AUMENTO APUNTADOR ACTUAL
+		blt  $s2, %tamaño, loop_shift		#SE REPITE HASTA QUE RECORRE TODO EL NUMERO
+.end_macro 
